5309221cd574ac4074bac0fb6eab144e9757ec81
BykovaJuliya <119175163+BykovaJuliya@users.noreply.github.com>
2023-05-17 00:40:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
-﻿// Задайте значение N. Напишите программу, которая выведет все чётные числа в 
-//промежутке от N до 1. Выполнить с помощью рекурсии.
-//N = 5 -> "4, 2"
-//N = 8 -> "8, 6, 4, 2,"
-Console.Write("Введите положительное число: ");
-int num = int.Parse(Console.ReadLine());
-int i = 1;
-EvenToLow(num, i);
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+//элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30;
 
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
 
-void EvenToLow(int n, int i)
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+SumMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumMToN(int m, int n)
 {
-    if (i > n)
-    {
-        return;
-    }
-    else 
-    {
-       if (n%2==0)
+    Console.Write(SumMN(m - 1, n));
+}
 
-        EvenToLow(n, i + 1);
-        Console.Write(i + " ");
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
     }
 }
