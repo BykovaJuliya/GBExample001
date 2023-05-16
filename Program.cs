772b1c58d5f2ea4a19eeb6e2ec64403989ab0cff
BykovaJuliya <119175163+BykovaJuliya@users.noreply.github.com>
2023-05-17 01:01:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
-﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+﻿// Задайте значение N. Напишите программу, которая выведет все чётные числа в 
+//промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "4, 2"
+//N = 8 -> "8, 6, 4, 2,"
+Console.Write("Введите положительное число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+EvenToLow(num, i);
+
+
+void EvenToLow(int n, int i)
+{
+    if (i > n)
+    {
+        return;
+    }
+    else 
+    {
+       if (n%2==0)
+
+        EvenToLow(n, i + 1);
+        Console.Write(i + " ");
+    }
+}
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
 //элементов в промежутке от M до N.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30;
@@ -28,5 +54,43 @@ int SumMN(int m, int n)
         m++;
         res = m + SumMN(m, n);
         return res;
+    }
+}
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write("A(m,n)="); 
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
     }
 }
